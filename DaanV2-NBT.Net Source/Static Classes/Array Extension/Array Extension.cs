7527df600d2c9712e6c428b0b748723fd027c00d
Daan Verstraten <Huntondoom@gmail.com>
2020-02-26 15:51:42 +0100

    Block rework

--- DaanV2-NBT.Net Source/Static Classes/Array Extension/Array Extension.cs
+++ DaanV2-NBT.Net Source/Static Classes/Array Extension/Array Extension.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 namespace DaanV2.NBT {
     /// <summary>The static class that extends upon existing array/collections</summary>
     public static partial class ArrayExtension {
+        /// <summary>Extends the clone mechanics of all the tags to collections</summary>
+        /// <param name="Values"></param>
+        /// <returns></returns>
         public static List<ITag> Clone(this List<ITag> Values) {
             Int32 Count = Values.Count;
             List<ITag> Out = new List<ITag>(Count);
