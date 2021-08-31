80e2c0df7552b8b5912808c5c3937bf32ebd24d9
DaanV2 <daanverstraten@hotmail.com>
2021-08-31 12:32:57 +0200

    Updated

--- Source/Enumerators/NBT Tag Type/NBT Tag Type.cs
+++ Source/Enumerators/NBT Tag Type/NBT Tag Type.cs
@@ -1,18 +1,6 @@
 ﻿/*ISC License
 
-Copyright (c) 2019, Daan Verstraten, daanverstraten@hotmail.com
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.*/
+Copyright (c) 2019, Daan Verstraten */
 using System;
 using System.Runtime.Serialization;
 
@@ -34,7 +22,7 @@ namespace DaanV2.NBT {
         Float = 5,
         ///<summary>Marks that the nbt tag stores a <see cref="System.Double"/> of information</summary>
         Double = 6,
-        ///<summary>Marks that the nbt tag stores a <see cref="System.Byte[]"/> of information</summary>
+        ///<summary>Marks that the nbt tag stores a <see cref="System.Byte"/>[] of information</summary>
         ByteArray = 7,
         ///<summary>Marks that the nbt tag stores a <see cref="System.String"/> of information</summary>
         String = 8,
@@ -42,9 +30,9 @@ namespace DaanV2.NBT {
         List = 9,
         ///<summary>Marks that the nbt tag stores a composite tag of named subtags</summary>
         Compound = 10,
-        ///<summary>Marks that the nbt tag stores a <see cref="Int32[]"/> of information</summary>
+        ///<summary>Marks that the nbt tag stores a <see cref="Int32"/>[] of information</summary>
         IntArray = 11,
-        ///<summary>Marks that the nbt tag stores a <see cref="Int64[]"/> of information</summary>
+        ///<summary>Marks that the nbt tag stores a <see cref="Int64"/>[] of information</summary>
         LongArray = 12,
         ///<summary>No idea what the tag should be</summary>
         Unknown = 255
